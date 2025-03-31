0af1984bce8002d492af6c2546ecb2aa551941fe
Aven Bross <kieroda@gmail.com>
2025-03-31 04:18:35 -0800

    Removed random ivec2_add_mod function

--- include/aven/math.h
+++ include/aven/math.h
@@ -871,14 +871,6 @@ static inline void ivec2_abs(IVec2 dst, IVec2 a) {
 #endif // AVEN_MATH_SIMD
 }
 
-static inline bool ivec2_add_mod(IVec2 dst, IVec2 a, IVec2 b, IVec2 c) {
-    IVec2 tmp;
-    ivec2_add(tmp, a, b);
-    ivec2_mod(dst, tmp, c);
-    ivec2_abs(dst, dst);
-    return (tmp[0] == dst[0]) and (tmp[1] == dst[1]);
-}
-
 static inline void imat2_copy(IMat2 dst, IMat2 m) {
 #ifdef AVEN_MATH_SIMD
     *(IVec4SIMD *)dst = *(IVec4SIMD *)m;
