a38179e433eacec34cdc9b5c6ba63fe368fcbd20
Aven Bross <kieroda@gmail.com>
2026-05-04 00:05:44 -0800

    Updated config.h

--- config.h
+++ config.h
@@ -3,8 +3,11 @@
 
 #if !defined(_WIN32) && defined(__GNUC__)
     #ifndef AVEN_BUILD_COMMON_DEFAULT_CFLAGS
-        #define AVEN_BUILD_COMMON_DEFAULT_CFLAGS \
-                    "-std=c11 -pedantic -fstrict-aliasing -O0 -g3 -Werror -Wall -Wextra " \
+        #define AVEN_BUILD_COMMON_DEFAULT_CFLAGS "-std=c11"
+    #endif
+    #ifndef AVEN_BUILD_COMMON_DEFAULT_CCFLAGS
+        #define AVEN_BUILD_COMMON_DEFAULT_CCFLAGS \
+                    "-pedantic -fstrict-aliasing -O0 -g3 -Werror -Wall -Wextra " \
                     "-Wshadow -Wconversion -Wdouble-promotion -Winit-self " \
                     "-Wcast-align -Wstrict-prototypes -Wold-style-definition " \
                     "-fsanitize-trap -fsanitize=unreachable -fsanitize=undefined"
